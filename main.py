0c807ec794ee0a8ed724b633d2517da1a72fa0fc
Nirmal <fernandonirmal345@gmail.com>
2025-01-20 23:25:02 +0530

    detect the person"s face

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import mediapipe as mp
 
 img = cv2.imread('portrait.jpg')
 
+H, W, _ = img.shape
+
 mp_face_detection = mp.solutions.face_detection
 
 with mp_face_detection.FaceDetection(model_selection=0, min_detection_confidence=0.5) as face_detection:
@@ -12,5 +14,16 @@ with mp_face_detection.FaceDetection(model_selection=0, min_detection_confidence
     for detection in out.detections:
         location_data = detection.location_data
         bbox = location_data.relative_bounding_box
+        
         x1, y1, w, h = bbox.xmin, bbox.ymin, bbox.width, bbox.height
         
+        x1 = int(x1 * W)
+        y1 = int(y1 * H)
+        w = int(w * W)
+        h = int(h * H)
+        
+        img = cv2.rectangle(img, (x1, y1), (x1 + w, y1 + h), (0, 255, 0), 10)
+        
+        cv2.imshow('img', img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows('img')
